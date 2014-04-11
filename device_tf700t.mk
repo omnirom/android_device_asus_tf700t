63680df6ba9aa1e06d8f47efca71b668518de2a7
pretoriano80 <pretoriano.mp@gmail.com>
2014-04-11 11:50:15 +0200

    ADD: download lto.dat to get quicker gps fix / get gps work (thx scanno the tf300t maintainer)
    Change-Id: Ia0c9d8cbebffd40a50ddbb8c14866dac64c6c407

--- device_tf700t.mk
+++ device_tf700t.mk
@@ -39,7 +39,8 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/audio_policy.conf:system/etc/audio_policy.conf \
     $(LOCAL_PATH)/configs/bt_vendor.conf:system/etc/bluetooth/bt_vendor.conf \
     $(LOCAL_PATH)/configs/wpa_supplicant_overlay.conf:system/etc/wifi/wpa_supplicant_overlay.conf \
-    $(LOCAL_PATH)/configs/p2p_supplicant_overlay.conf:system/etc/wifi/p2p_supplicant_overlay.conf
+    $(LOCAL_PATH)/configs/p2p_supplicant_overlay.conf:system/etc/wifi/p2p_supplicant_overlay.conf \
+    $(LOCAL_PATH)/get-gps-lto:system/bin/get-gps-lto
 
 # Input device configuration files
 PRODUCT_COPY_FILES += \
