94338b515fca7e6c3296eb1b15a698c85e0c4181
Josh <jl52741@gmail.com>
2014-11-23 17:26:22 -0600

    tf700t: 4.4 bringup
    PS2: Remove as it is not needed
    PS3: Change Copyrights
    PS4: Lets get all the Copyrights :)
    
    Change-Id: I929eead074e4fe893cfca7434312e44c85bb8823

--- libhealthd/Android.mk
+++ libhealthd/Android.mk
@@ -1,5 +1,5 @@
-#
-# Copyright (C) 2011 The Android Open Source Project
+# Copyright (C) 2013 The Android Open Source Project
+# Copyright (C) 2013 The CyanogenMod Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -12,6 +12,16 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-#
-add_lunch_combo full_tf700t-userdebug
-add_lunch_combo omni_tf700t-userdebug
+
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+
+LOCAL_SRC_FILES := healthd_board_cardhu.cpp
+LOCAL_C_INCLUDES := system/core/healthd
+
+LOCAL_STATIC_LIBRARIES := libutils
+
+LOCAL_MODULE := libhealthd.cardhu
+
+include $(BUILD_STATIC_LIBRARY)
