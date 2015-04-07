23c8bedfc179d7e5db569ae1a28d4e91a0760761
lj50036 <jl52741@gmail.com>
2015-04-07 09:54:30 -0500

    tf700t: Transformer devices do not have a telephony subsystem.
    Change-Id: I4a6383c6b74e946202b93d6818a703d714e01fff

--- device_tf700t.mk
+++ device_tf700t.mk
@@ -112,7 +112,6 @@ PRODUCT_PROPERTY_OVERRIDES += \
 # media files
 PRODUCT_COPY_FILES += \
     frameworks/av/media/libstagefright/data/media_codecs_google_audio.xml:system/etc/media_codecs_google_audio.xml \
-    frameworks/av/media/libstagefright/data/media_codecs_google_telephony.xml:system/etc/media_codecs_google_telephony.xml \
     frameworks/av/media/libstagefright/data/media_codecs_google_video.xml:system/etc/media_codecs_google_video.xml \
     device/asus/tf700t/media_codecs.xml:system/etc/media_codecs.xml \
     device/asus/tf700t/media_profiles.xml:system/etc/media_profiles.xml \
